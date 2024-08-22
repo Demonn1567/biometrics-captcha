2f640bb28452a225d7a6fd58c353d4958a169c1e
Demonn1567 <krishsharmacool3@gmail.com>
2024-08-22 18:38:55 +0530

    cors-update

--- biometrics/biometrics/settings.py
+++ biometrics/biometrics/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uv1o3ma1g9^s0!3)!um%)c(-x88p5@kg0)z8lko5lws-e96j*i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'userbehavior',
-    'corsheaders'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -131,4 +131,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'https://biometrics-movements.vercel.app/',
+]
+
